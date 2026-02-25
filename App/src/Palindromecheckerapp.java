af856e3c9086ee3ebcc732195eab802d29a468a1
chaitanya <chaitanyakotha777@gmail.com>
2026-02-25 11:25:54 +0530

    Chaitanya 1124 uc2

--- App/src/Palindromecheckerapp.java
+++ App/src/Palindromecheckerapp.java
@@ -2,5 +2,15 @@
 public class Palindromecheckerapp{
     public static void main(String[] args) {
    System.out.print("WELCOME");
-    }
-}
+                String word = "tenet";
+                String reversed = "";
+                for (int i = word.length() - 1; i >= 0; i--) {
+                    reversed = reversed + word.charAt(i);
+                }
+                if (word.equals(reversed)) {
+                    System.out.println(word + " is a Palindrome.");
+                } else {
+                    System.out.println(word + " is NOT a Palindrome.");
+                }
+            }
+       }
